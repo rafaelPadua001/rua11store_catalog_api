166d59a125558d864e3f80058e0ad910b7d69ad3
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2025-03-31 00:37:35 -0300

    Revert "Initial commit"
    This reverts commit 1c78e9f61a7023e197a2a9229aec02288e37a311.

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "chave_secreta")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
