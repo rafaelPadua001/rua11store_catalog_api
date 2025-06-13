7db1a56ef41f7a57b86da9e5224d30d7d6f5fc12
Rafael Pádua <rafael.f.p.faria@hotmail.com>
2025-06-13 00:12:49 -0300

    fix
    fix

--- backend/models/userProfile.py
+++ backend/models/userProfile.py
@@ -31,16 +31,16 @@ class UserProfile:
         self.avatar_url = avatar_url
         self.name = name if name else full_name
 
-class User(db.Model):
-    __tablename__ = 'users'
+# class User(db.Model):
+#     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    name = db.Column(db.String, nullable=False)
-    birth_date = db.Column(db.Date, nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String, unique=True, nullable=False)
+#     password = db.Column(db.String, nullable=False)
+#     name = db.Column(db.String, nullable=False)
+#     birth_date = db.Column(db.Date, nullable=False)
 
-    profile = db.relationship('ProfileModel', back_populates='user', uselist=False)
+#     profile = db.relationship('ProfileModel', back_populates='user', uselist=False)
 
 
 class ProfileModel(db.Model):
