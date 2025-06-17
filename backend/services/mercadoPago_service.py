16123d48060093fad2fba46df59bf20f089cae80
Rafael Pádua <rafael.f.p.faria@hotmail.com>
2025-06-17 12:49:21 -0300

    Fix
    Fix

--- backend/services/mercadoPago_service.py
+++ backend/services/mercadoPago_service.py
@@ -64,11 +64,11 @@ class CreditCardPayment(PaymentStrategy):
                 "items": [
                     {
                         "id": item.get("id", "SKU123"),
-                        "title": item.get("product_name", "Produto"),
+                        "title": item.get("title", "Produto"),
                         "description": item.get("description", "Produto comprado"),
-                        "category_id": str(item.get("category", "services")),
+                        "category_id": item.get("category", "services"),
                         "quantity": item.get("quantity", 1),
-                        "unit_price": float(item.get("price", data["total"]))  # fallback para valor total
+                        "unit_price": float(item.get("unit_price", data["total"]))  # fallback para valor total
                     }
                     for item in data.get("products", [])
                 ],
@@ -80,14 +80,9 @@ class CreditCardPayment(PaymentStrategy):
                         "number": data.get("phone_number", "999999999")
                     },
                     "address": {
-                        "zip_code": data.get("address", {}).get("zip_code", "00000-000"),
-                        "street_name": data.get("address", {}).get("street", "Rua Exemplo"),
-                        "street_number": str(data.get("address", {}).get("number", "123")),
-                        "neighborhood": data.get("address", {}).get("bairro", "Centro"),
-                        "city_name": data.get("address", {}).get("city", "Cidade Exemplo"),
-                        "state": data.get("address", {}).get("state", "DF"),
-                        "neighborhood": data.get("address", {}).get("bairro", "Bairro Exemplo"),
-                        "comment": data.get("address", {}).get("complement", "")
+                        "zip_code": data.get("zip_code", "00000-000"),
+                        "street_name": data.get("street_name", "Rua Exemplo"),
+                        "street_number": data.get("street_number", 123)
                     }
                 }
             }
