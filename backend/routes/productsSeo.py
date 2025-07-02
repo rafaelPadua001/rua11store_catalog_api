45fc1e87b468cc4470c75d7f66ce78a229ad3865
Rafael Pádua <rafael.f.p.faria@hotmail.com>
2025-07-02 15:41:23 -0300

    Feat
    Create sitemap, generate sitemap, fix productSeoController to list product seo

--- backend/routes/productsSeo.py
+++ backend/routes/productsSeo.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from controllers.productSeoController import ProductSeoController
 
 product_seo_bp = Blueprint('product_seo', __name__)
@@ -7,3 +7,9 @@ product_seo_bp = Blueprint('product_seo', __name__)
 def create_product_seo():
     data = request.get_json()
     return ProductSeoController.create_product_seo(data)
+
+@product_seo_bp.route('/', methods=['GET'])
+def list_product_seo():
+    # Aqui deve retornar a lista de produtos com slug para o sitemap
+    produtos = ProductSeoController.list_product_seo()
+    return jsonify(produtos)
