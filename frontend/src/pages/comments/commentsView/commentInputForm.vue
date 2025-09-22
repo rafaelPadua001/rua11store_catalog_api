4ec5a1fa249cb0f2daa4c57598fbebacd0ad3556
Rafael Pádua <145411939+rafaelPadua001@users.noreply.github.com>
2025-09-22 18:47:57 -0300

    Update commentInputForm.vue
    Update currentUrl

--- frontend/src/pages/comments/commentsView/commentInputForm.vue
+++ frontend/src/pages/comments/commentsView/commentInputForm.vue
@@ -117,7 +117,7 @@ const clearComment = () => {
 //Social login
 const login = async (provider) => {
     try {
-        const currentUrl = window.location.href; // pega a URL atual (com o slug)
+        const currentUrl = window.location.origin + this.$route.fullPath; // pega a URL atual (com o slug)
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider,
             options: {
